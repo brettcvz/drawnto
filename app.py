a3a0d26198143a0eb27bc9974f0fa466b3780b7d
Brett van Zuiden <brettcvz@gmail.com>
2013-03-23 13:59:08 -0700

    Setup for heroku

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+app.DEBUG = True
+app.static_folder = 'static'  # Enable is back, but the URL rule is
+app.add_url_rule('/static/<path:filename>',
+                 endpoint='static',
+                 view_func=app.send_static_file)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
